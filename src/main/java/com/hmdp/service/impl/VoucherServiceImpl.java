18d49aba22131a77c9da61d8473c2ae901f1082b
hbycgithub2 <1311768852@qq.com>
2025-03-23 20:37:26 +0800

    优惠券秒杀3-超卖问题

--- src/main/java/com/hmdp/service/impl/VoucherServiceImpl.java
+++ src/main/java/com/hmdp/service/impl/VoucherServiceImpl.java
@@ -81,7 +81,9 @@ public class VoucherServiceImpl extends ServiceImpl<VoucherMapper, Voucher> impl
         }
 
         //5.扣减库存
-        boolean success = seckillVoucherService.update().setSql("stock =stock-1").eq("voucher_id", voucherId).update();
+        boolean success = seckillVoucherService.update().setSql("stock =stock-1")
+                .eq("voucher_id", voucherId).gt("stock", 0) // where voucher_i = ？ and stock > 0
+                .update();
         if (!success) {
             return Result.fail("库存不足！");
         }
