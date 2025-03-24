dc60a6672bf4b542240253d6132a12c799165eff
hbycgithub2 <1311768852@qq.com>
2025-03-24 12:45:41 +0800

    优惠券秒杀6-Redis的Lua脚本

--- src/main/java/com/hmdp/utils/SimpleRedisLock.java
+++ src/main/java/com/hmdp/utils/SimpleRedisLock.java
@@ -1,7 +1,10 @@
 package com.hmdp.utils;
 
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 
+import java.util.Collections;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
@@ -18,6 +21,13 @@ public class SimpleRedisLock implements ILock{
     private static final String KEY_PREFIX = "lock";
     private static final String ID_PREFIX = UUID.randomUUID().toString() + "-";
 
+    private static final DefaultRedisScript<Long> UNLOCK_SCRIPT;
+
+    static {
+        UNLOCK_SCRIPT = new DefaultRedisScript<>();
+        UNLOCK_SCRIPT.setLocation(new ClassPathResource("unlock.lua"));
+        UNLOCK_SCRIPT.setResultType(Long.class);
+    }
     @Override
     public boolean tryLock(long timeoutSec) {
         //获取线程标识
@@ -29,6 +39,13 @@ public class SimpleRedisLock implements ILock{
 
     @Override
     public void unlock() {
+        // 调用lua脚本
+        stringRedisTemplate.execute(UNLOCK_SCRIPT,
+                Collections.singletonList(KEY_PREFIX + name),
+                ID_PREFIX + Thread.currentThread().getId());
+    }
+   /* @Override
+    public void unlock() {
         // 获取线程标示
         String threadId =ID_PREFIX + Thread.currentThread().getId();
         // 获取锁中的标示
@@ -38,5 +55,5 @@ public class SimpleRedisLock implements ILock{
             // 释放锁完成
             stringRedisTemplate.delete(KEY_PREFIX + name);
         }
-    }
+    }*/
 }
